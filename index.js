06d3719b366c76719679f11ed3cca26745a57b1e
Cabsifontes <cabs.sifontes@gmail.com>
2026-04-23 19:22:45 -0300

    configuración de conexión a mongodb atlas y variables de entorno

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const app = express();
-
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Conectado a MongoDB"))
+  .catch((err) => console.error("Error al conectar a MongoDB:", err));
 
 app.get("/", (req, res) => {
   res.send(
