fcc521e3424915e39933695cb68b1e83ed0e4c9f
Cabsifontes <cabs.sifontes@gmail.com>
2026-05-05 22:44:29 -0300

    cambio de lógica en búsqueda y mejora en la respuesta de la API

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,17 +5,12 @@ const jwt = require("jsonwebtoken");
 exports.register = async (req, res) => {
   try {
     const { nombre, email, password } = req.body;
-
     let user = await User.findOne({ email });
     if (user) return res.status(400).json({ msg: "L'utente esiste già" });
-
     user = new User({ nombre, email, password });
-
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(password, salt);
-
     await user.save();
-
     res.status(201).json({ msg: "Utente registrato correctamente" });
   } catch (err) {
     res.status(500).send("Errore del server");
@@ -24,24 +19,27 @@ exports.register = async (req, res) => {
 
 exports.login = async (req, res) => {
   try {
-    const { nombre, password } = req.body;
-
-    const user = await User.findOne({ nombre });
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ msg: "Credenziali non valide" });
-
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
       return res.status(400).json({ msg: "Credenziali non valide" });
-
     const payload = { user: { id: user.id, role: user.role } };
-
     jwt.sign(
       payload,
       process.env.JWT_SECRET,
       { expiresIn: "1h" },
       (err, token) => {
         if (err) throw err;
-        res.json({ token, user: { nombre: user.nombre, role: user.role } });
+        res.json({
+          token,
+          user: {
+            id: user.id,
+            nombre: user.nombre,
+            role: user.role,
+          },
+        });
       },
     );
   } catch (err) {
