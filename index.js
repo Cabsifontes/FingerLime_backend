23baa0ca816f8e3440e8878b4b11af6104d37fd1
Cabsifontes <cabs.sifontes@gmail.com>
2026-05-15 20:14:21 -0300

    Se integraron las rutas del carrito de compras en el servidor principal

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const emailRoutes = require("./src/routes/email.routes");
 const authRoutes = require("./src/routes/auth.routes");
 const productRoutes = require("./src/routes/product.routes");
+const cartRoutes = require("./src/routes/cart.routes"); 
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ mongoose
 app.use("/api/email", emailRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
