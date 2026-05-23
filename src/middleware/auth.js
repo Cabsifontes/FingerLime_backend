aa224bf28cd383f481505c2f44988dde4fa74ee7
Cabsifontes <cabs.sifontes@gmail.com>
2026-05-23 17:16:50 -0300

    Corrección autenticación y creación de órdenes en checkout

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,22 +4,27 @@ const auth = (req, res, next) => {
   const token = req.header("Authorization")?.replace("Bearer ", "");
 
   if (!token) {
-    return res.status(401).json({ msg: "Nessun token, autorizzazione negata" });
+    return res.status(401).json({
+      msg: "Nessun token, autorizzazione negata",
+    });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     req.user = decoded.user;
 
     if (req.user.role !== "admin") {
-      return res
-        .status(403)
-        .json({ msg: "Accesso negato: non sei un amministratore" });
+      return res.status(403).json({
+        msg: "Accesso negato: non sei un amministratore",
+      });
     }
 
     next();
   } catch (err) {
-    res.status(401).json({ msg: "Il token non è valido" });
+    res.status(401).json({
+      msg: "Il token non è valido",
+    });
   }
 };
 
@@ -27,16 +32,21 @@ const userAuth = (req, res, next) => {
   const token = req.header("Authorization")?.replace("Bearer ", "");
 
   if (!token) {
-    return res.status(401).json({ msg: "Nessun token, autorizzazione negata" });
+    return res.status(401).json({
+      msg: "Nessun token, autorizzazione negata",
+    });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     req.user = decoded.user;
 
     next();
   } catch (err) {
-    res.status(401).json({ msg: "Il token non è valido" });
+    res.status(401).json({
+      msg: "Il token non è valido",
+    });
   }
 };
 
